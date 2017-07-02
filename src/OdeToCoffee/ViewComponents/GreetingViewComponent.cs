e23dcfd4da35fa3107c9240986668d4f8d6311c9
Przemek Owsianik <Przemek.Owsianik@gmail.com>
2017-07-02 12:03:43 +0200

    Idea of View Components

---
+++ src/OdeToCoffee/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToCoffee.Services;
+
+namespace OdeToCoffee.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        private IMessageGetter messageGetter;
+
+        public GreetingViewComponent(IMessageGetter messageGetter)
+        {
+            this.messageGetter = messageGetter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = this.messageGetter.GetMessage();
+
+            return View("Default", model);
+        }
+    }
+}
